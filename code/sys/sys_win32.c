85f2b8aa19792990c9a574d1a16280214d92a1ad
Zack Middleton <zack@cloemail.com>
2025-12-11 21:25:59 -0600

    Fix arguments for SHGetFolderPathA function pointer
    This was using FARPROC which doesn't define arguments but newer mingw
    throws an error.

--- code/sys/sys_win32.c
+++ code/sys/sys_win32.c
@@ -42,6 +42,13 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <psapi.h>
 #include <float.h>
 
+// include definition of PFNSHGETFOLDERPATHA
+#ifdef _MSC_VER
+#include <shlobj_core.h>
+#else
+#include <shfolder.h>
+#endif
+
 #ifndef KEY_WOW64_32KEY
 #define KEY_WOW64_32KEY 0x0200
 #endif
@@ -103,7 +110,7 @@ Sys_DefaultHomePath
 char *Sys_DefaultHomePath( void )
 {
 	TCHAR szPath[MAX_PATH];
-	FARPROC qSHGetFolderPath;
+	PFNSHGETFOLDERPATHA qSHGetFolderPath;
 	HMODULE shfolder = LoadLibrary("shfolder.dll");
 
 	if(shfolder == NULL)
@@ -114,7 +121,7 @@ char *Sys_DefaultHomePath( void )
 
 	if(!*homePath && com_homepath)
 	{
-		qSHGetFolderPath = GetProcAddress(shfolder, "SHGetFolderPathA");
+		qSHGetFolderPath = (PFNSHGETFOLDERPATHA) GetProcAddress(shfolder, "SHGetFolderPathA");
 		if(qSHGetFolderPath == NULL)
 		{
 			Com_Printf("Unable to find SHGetFolderPath in SHFolder.dll\n");
@@ -234,7 +241,7 @@ char* Sys_MicrosoftStorePath(void)
 	if (!microsoftStorePath[0]) 
 	{
 		TCHAR szPath[MAX_PATH];
-		FARPROC qSHGetFolderPath;
+		PFNSHGETFOLDERPATHA qSHGetFolderPath;
 		HMODULE shfolder = LoadLibrary("shfolder.dll");
 
 		if(shfolder == NULL)
@@ -243,7 +250,7 @@ char* Sys_MicrosoftStorePath(void)
 			return microsoftStorePath;
 		}
 
-		qSHGetFolderPath = GetProcAddress(shfolder, "SHGetFolderPathA");
+		qSHGetFolderPath = (PFNSHGETFOLDERPATHA) GetProcAddress(shfolder, "SHGetFolderPathA");
 		if(qSHGetFolderPath == NULL)
 		{
 			Com_Printf("Unable to find SHGetFolderPath in SHFolder.dll\n");
